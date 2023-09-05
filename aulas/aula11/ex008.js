a91934313b8defa8db68bf3d23c5f3d50f7d7464
Erick Kohn <133143910+kohnduarte@users.noreply.github.com>
2023-09-05 13:40:28 -0300

    exercícios 8 e 9
    Condições em JS

---
+++ aulas/aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 30
+vel += 70
+console.log('Boa tarde!')
+console.log('Sua velocidade é ' + vel + 'km/h.')
+if(vel > 60) { // condição simples
+    console.log('Você ultrapassou o limite de velocidade. MULTADO!')
+}
+console.log('Use sempre o cinto de segurança.')
+console.log('Boa viagem!')
---
+++ aulas/aula11/ex009.js
@@ -0,0 +1,11 @@
+país = 'Espanha'
+
+console.log('Seu país: ' + país)
+
+if(país != 'Brasil') {
+    console.log('Você é estrangeiro')
+    console.log('Você não nasceu no Brasil')
+} else {
+    console.log('Você é brasileiro')
+    console.log('Você nasceu aqui')
+}
