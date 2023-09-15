be8bd94b2582674af14185c649ea8ead7bc50d98
Erick Kohn <133143910+kohnduarte@users.noreply.github.com>
2023-09-14 21:29:39 -0300

    update

--- aulas/aula12ex/ex014-b/javascript/script.js
+++ aulas/aula12ex/ex014-b/javascript/script.js
@@ -1,7 +1,7 @@
 var hora = new Date().getHours()
 var min = new Date().getMinutes()
 var segs = new Date().getSeconds()
-var hora = 6
+
 var B = window.document.body
 var P = window.document.getElementById('parh')
 var i = window.document.getElementsByTagName('img')[0]
@@ -13,5 +13,14 @@ if (hora >= 0 && hora < 6) {
     i.src = 'img/noite.png'
 } else if (hora < 12) {
     i.src = 'img/manha.png'
-    
+    B.style.backgroundColor = '#eff156'
+} else if (hora < 16) {
+    i.src = 'img/tarde.png'
+    B.style.backgroundColor = '#4283dd'
+} else if (hora < 19) {
+    i.src = 'img/sunset.png'
+    B.style.backgroundColor = 'darkorange'
+} else {
+    i.src = 'img/noite.png'
+    B.style.background = 'url(img/night.jpg)'
 }
