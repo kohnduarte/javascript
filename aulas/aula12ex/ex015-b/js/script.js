0934f23b33bb143221a132484f1a8788589b6cd7
Erick Kohn <133143910+kohnduarte@users.noreply.github.com>
2023-09-18 13:52:55 -0300

    Update script.js

--- aulas/aula12ex/ex015-b/js/script.js
+++ aulas/aula12ex/ex015-b/js/script.js
@@ -10,12 +10,14 @@ function Verificar() {
     var radio = window.document.getElementsByName('genero')
     
     var P = window.document.querySelector('p#divres')
-    
+    var D = window.document.getElementsByClassName('resultado')[0]
 
     if (radio[0].checked) {
         var gen = 'Homem'
         if (idade < 10) {
-            I.setAttribute('src', 'img/h-00-09.png')
+            I.src = 'img/h-00-09.png'
+        } else if (idade < 20) {
+            I.setAttribute('src', 'img/h-10-19.png')
         }
     } else {
         var gen = 'Mulher'
@@ -24,9 +26,8 @@ function Verificar() {
     P.innerText = `Detectamos ${gen} com ${idade} anos.`
     D.style.textAlign = 'center'
     P.style.marginBottom = '1em'
-    
+    I.setAttribute('alt', 'imagem que corresponde ao gênero e à idade da pessoa')
+    D.appendChild(I)
 }
 
-var D = window.document.getElementsByClassName('resultado')[0]
 var I = window.document.createElement('img')
-D.appendChild(I)
