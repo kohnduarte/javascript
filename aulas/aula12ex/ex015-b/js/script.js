a9ad6fc739f3d64c91b18f2a0a22a19ae6203da5
Erick Kohn <133143910+kohnduarte@users.noreply.github.com>
2023-09-19 13:24:28 -0300

    projeto finalizado

--- aulas/aula12ex/ex015-b/js/script.js
+++ aulas/aula12ex/ex015-b/js/script.js
@@ -7,44 +7,48 @@ function Verificar() {
 
         // gênero e imagem
 
-    var radio = window.document.getElementsByName('genero')
-    
-    var P = window.document.querySelector('p#divres')
+    var radio = window.document.getElementsByName('genero')    
     var D = window.document.getElementsByClassName('resultado')[0]
 
-    if (radio[0].checked) {
-        var gen = 'Homem'
-        if (idade < 10) {
-            I.src = 'img/h-00-09.png'
-        } else if (idade < 20) {
-            I.setAttribute('src', 'img/h-10-19.png')
-        } else if (idade < 40) {
-            I.src = 'img/h-20-40.png'
-        } else if (idade < 65) {
-            I.setAttribute('src', 'img/h-41-64.png')
+    if (nasceu <= anoat && nasceu >= 1900) {        
+        if (radio[0].checked) {
+            var gen = 'Homem'
+            if (idade < 10) {
+                I.setAttribute('src', 'img/h-00-09.png')
+            } else if (idade < 20) {
+                I.src = 'img/h-10-19.png'
+            } else if (idade < 40) {
+                I.setAttribute('src', 'img/h-20-40.png')
+            } else if (idade < 65) {
+                I.src = 'img/h-41-64.png'
+            } else {
+                I.setAttribute('src', 'img/h-65-xxx.png')
+            }
         } else {
-            I.src = 'img/h-65-xxx.png'
+            var gen = 'Mulher'
+            if (idade < 10) {
+                I.src = 'img/m-00-09.png'
+            } else if (idade < 20) {
+                I.setAttribute('src', 'img/m-10-19.png')
+            } else if (idade < 40) {
+                I.src = 'img/m-20-40.png'
+            } else if (idade < 65) {
+                I.setAttribute('src', 'img/m-41-64.png')
+            } else {
+                I.src = 'img/m-65-xxx.png'
+            }
         }
+        D.innerHTML = `<p style="margin-bottom: 1em;">Detectamos ${gen} com ${idade} anos.</p>`
+        D.appendChild(I)
     } else {
-        var gen = 'Mulher'
-        if (idade < 10) {
-            I.setAttribute('src', 'img/m-00-09.png')
-        } else if (idade < 20) {
-            I.src = 'img/m-10-19.png'
-        } else if (idade < 40) {
-            I.setAttribute('src', 'img/m-20-40.png')
-        } else if (idade < 65) {
-            I.src = 'img/m-41-64.png'
-        } else {
-            I.setAttribute('src', 'img/m-65-xxx.png')
-        }
+        window.alert('[ERRO] Ano inválido!')
+        D.innerHTML = '<p>Por favor, insira um ano válido e tente novamente.</p>'
     }
 
-    P.innerText = `Detectamos ${gen} com ${idade} anos.`
-    D.style.textAlign = 'center'
-    P.style.marginBottom = '1em'
+    
+    D.style.textAlign = 'center'    
     I.setAttribute('alt', 'imagem que corresponde ao gênero e à idade da pessoa')
-    D.appendChild(I)
+    
 }
 
 var I = window.document.createElement('img')
