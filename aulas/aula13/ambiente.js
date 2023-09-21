08d9d104f7f68bb498e0daeb3dee1a7e146692b1
Erick Kohn <133143910+kohnduarte@users.noreply.github.com>
2023-09-21 13:27:05 -0300

    Repetições

---
+++ aulas/aula13/ambiente.js
@@ -0,0 +1,19 @@
+/*fatia = 8
+
+while (fatia >= 5) {
+    fatia-- // fatia = fatia - 1 ou fatia -= 1
+    console.log('comi 1 fatia')
+}
+
+if (fatia < 1) {
+    console.log('comi a pizza inteira')
+} else {
+    console.log(`não comi a pizza inteira, deixei ${fatia} fatia(s)`)
+}*/
+
+var c = 20
+
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
---
+++ pratica/p009.js
@@ -0,0 +1,22 @@
+var fatia = 2
+
+while (fatia >= 3) {
+    fatia--
+    console.log('comi 1 fatia')
+}
+
+if (fatia < 1) {
+    console.log('comi a pizza inteira')
+} else {
+    console.log('não comi a pizza inteira, deixei ' + fatia + ' fatia(s)')
+}
+
+/*var fatia = 5
+
+do {
+    // o bloco é executado 1 vez antes da expressão ser avaliada
+    fatia--
+    console.log('comi 1 fatia')
+} while (fatia >= 30)
+
+console.log('quantidade que sobrou: ' + fatia + ' fatias')*/
