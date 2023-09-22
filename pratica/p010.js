03988b8cbee1c053284424787424419486a32048
Erick Kohn <133143910+kohnduarte@users.noreply.github.com>
2023-09-21 21:12:10 -0300

    Create p010.js

---
+++ pratica/p010.js
@@ -0,0 +1,17 @@
+var n = 0
+
+console.log(n)
+
+while (n < 100) {
+    n++ // n = n + 1 ou n += 1
+    console.log(n)
+}
+
+console.log('Chegamos no número ' + n + '!!!')
+
+while (n <= 100 && n > 0) {
+    n--
+    console.log(n)
+}
+
+console.log('Voltamos ao número ' + n + '!!!')
