8f43c77d29f48aa2ef920d330a78d8fe677fc9dd
Erick Kohn <133143910+kohnduarte@users.noreply.github.com>
2023-09-23 12:51:59 -0300

    update

---
+++ aulas/aula14ex/ex016/js/script.js
@@ -0,0 +1,39 @@
+function contar() {
+    var cinicio = window.document.getElementById('c1')
+    var cfim = window.document.querySelector('input#c2')
+    var cpasso = window.document.getElementsByName('passo')[0]
+
+    var start = Number.parseInt(cinicio.value)
+    var end = parseInt(cfim.value)
+    var step = Number.parseInt(cpasso.value)
+
+    var D = window.document.getElementsByTagName('div')[0]
+    var P = window.document.getElementsByClassName('change')[0]
+
+    P.style.display = 'none'
+
+    var P2 = window.document.createElement('p')
+    D.appendChild(P2)
+
+    for (n = start; n <= end; n++) {
+        P2.innerText += ' ' + n + ' 👉'
+    }
+
+    P2.innerText += ' 🏳️FIM🏴'
+
+    
+
+    R.setAttribute('type', 'button')
+    R.setAttribute('value', 'Apagar')
+    R.setAttribute('onclick', 'apaga()')
+    D.appendChild(R)
+
+
+
+}
+
+var R = window.document.createElement('input')
+
+function apaga() {
+    P2.style.display = 'none'
+}
