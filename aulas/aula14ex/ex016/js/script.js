68c8d287006c9507a2555b589ac4e5045ee35acb
Erick Kohn <133143910+kohnduarte@users.noreply.github.com>
2023-09-23 17:31:26 -0300

    Update script.js

--- aulas/aula14ex/ex016/js/script.js
+++ aulas/aula14ex/ex016/js/script.js
@@ -8,9 +8,7 @@ function contar() {
     var passo = Number.parseInt(cpasso.value)
 
     var D = window.document.getElementsByTagName('div')[0]
-    var P = window.document.getElementsByClassName('change')[0]
-
-    
+    var P = window.document.getElementsByClassName('change')[0]    
 
     if (start >= -100 && start <= 100 && end <= 100 && passo >= 1 && passo <= 10) {
         P.innerText = null
@@ -21,9 +19,7 @@ function contar() {
     } else {
         window.alert('[ERRO] Por favor, preencha os campos novamente')
         P.innerText = 'Preparando a contagem...'
-    }
-
-    
+    }    
 
 }
 
