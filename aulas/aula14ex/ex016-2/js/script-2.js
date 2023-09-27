4192c8b010a36ee9872f740db28b6ba5f3b0f1ed
Erick Kohn <133143910+kohnduarte@users.noreply.github.com>
2023-09-27 13:36:48 -0300

    Update script-2.js

--- aulas/aula14ex/ex016-2/js/script-2.js
+++ aulas/aula14ex/ex016-2/js/script-2.js
@@ -11,14 +11,13 @@ var passo = parseInt(cpasso.value)
 var P = window.document.getElementsByClassName('change')[0]
 
 if (cini.value.length > 0 && cfim.value.length > 0 && cpasso.value.length > 0) {
-    if (passo > 0) {
-        P.innerText = ''
-        for (n = start; n <= end; n += passo) {
-            P.innerHTML += n
-        }
-    } else {
-        window.alert(`[ERRO] Impossível contar de ${passo} em ${passo}! Considerarei passo 1`)
+    if (passo == 0) {
+        window.alert('[ERRO]')
         passo = 1
+    }
+    P.innerText = ''
+    for (n = start; n <= end; n += passo) {
+        P.innerHTML += n
     }
 } else {
     window.alert('[ERRO] Faltam dados!')
