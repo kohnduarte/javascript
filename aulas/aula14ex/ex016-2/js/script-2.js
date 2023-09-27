fbc7977f2ad7cb2821461387d584123f8311f8f0
Erick Kohn <133143910+kohnduarte@users.noreply.github.com>
2023-09-27 13:30:51 -0300

    update

---
+++ aulas/aula14ex/ex016-2/js/script-2.js
@@ -0,0 +1,32 @@
+function contar() {
+
+var cini = window.document.getElementById('c1')
+var cfim = window.document.querySelector('input#c2')
+var cpasso = window.document.getElementsByName('passo')[0]
+
+var start = Number.parseInt(cini.value)
+var end = parseInt(cfim.value)
+var passo = parseInt(cpasso.value)
+
+var P = window.document.getElementsByClassName('change')[0]
+
+if (cini.value.length > 0 && cfim.value.length > 0 && cpasso.value.length > 0) {
+    if (passo > 0) {
+        P.innerText = ''
+        for (n = start; n <= end; n += passo) {
+            P.innerHTML += n
+        }
+    } else {
+        window.alert(`[ERRO] Impossível contar de ${passo} em ${passo}! Considerarei passo 1`)
+        passo = 1
+    }
+} else {
+    window.alert('[ERRO] Faltam dados!')
+}
+
+
+
+
+
+
+}
