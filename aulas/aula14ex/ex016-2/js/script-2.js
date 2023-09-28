0a8f4863f49f859175a3bc1391fd9aaa394e969e
Erick Kohn <133143910+kohnduarte@users.noreply.github.com>
2023-09-28 20:51:46 -0300

    Update script-2.js

--- aulas/aula14ex/ex016-2/js/script-2.js
+++ aulas/aula14ex/ex016-2/js/script-2.js
@@ -12,15 +12,17 @@ var P = window.document.getElementsByClassName('change')[0]
 
 if (cini.value.length > 0 && cfim.value.length > 0 && cpasso.value.length > 0) {
     if (passo == 0) {
-        window.alert('[ERRO]')
+        window.alert('[ERRO] Impossível contar de ' + passo + ' em ' + passo + '! Considerarei passo 1')
         passo = 1
     }
     P.innerText = ''
     for (n = start; n <= end; n += passo) {
-        P.innerHTML += n
+        P.innerHTML += n + ' \u{1F449} '
+        // inserindo emojis com JavaScript: \u{código-do-emoji}
     }
 } else {
     window.alert('[ERRO] Faltam dados!')
+    
 }
 
 
