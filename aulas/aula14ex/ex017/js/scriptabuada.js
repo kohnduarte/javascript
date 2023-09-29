55882ee834996277291d8de69849f40e0e138fe7
Erick Kohn <133143910+kohnduarte@users.noreply.github.com>
2023-09-29 16:33:20 -0300

    update

--- aulas/aula14ex/ex017/js/scriptabuada.js
+++ aulas/aula14ex/ex017/js/scriptabuada.js
@@ -1,19 +1,16 @@
 function tabuada() {
     var caixa = window.document.getElementById('c1')
-
+    var S = window.document.querySelector('select#sel')
+    var O = window.document.getElementsByName('remover')[0]
     if (caixa.value.length > 0) {
         var n = Number.parseInt(caixa.value)
         if (n >= 0 && n <= 10) {
-
-
-
-
-
-
-
-
-            
-            
+            O.style.display = 'none'
+            for (t = 1; t <= 10; t++) {
+                var opt = window.document.createElement('option')
+                S.appendChild(opt)
+                opt.innerText = `${n} x ${t} = ${n*t}`
+            }            
         } else {
             window.alert('[ERRO] O número deve ser entre 0 e 10.')
         }
