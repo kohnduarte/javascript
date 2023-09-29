62af0c04521be0cee465ac634589f5054092798b
Erick Kohn <133143910+kohnduarte@users.noreply.github.com>
2023-09-29 17:53:28 -0300

    fim do exercício

--- aulas/aula14ex/ex017/js/scriptabuada.js
+++ aulas/aula14ex/ex017/js/scriptabuada.js
@@ -1,11 +1,11 @@
 function tabuada() {
     var caixa = window.document.getElementById('c1')
     var S = window.document.querySelector('select#sel')
-    var O = window.document.getElementsByName('remover')[0]
+        
     if (caixa.value.length > 0) {
         var n = Number.parseInt(caixa.value)
-        if (n >= 0 && n <= 10) {
-            O.style.display = 'none'
+        if (n >= 0 && n <= 10) {            
+            S.innerHTML = ''
             for (t = 1; t <= 10; t++) {
                 var opt = window.document.createElement('option')
                 S.appendChild(opt)
@@ -13,8 +13,10 @@ function tabuada() {
             }            
         } else {
             window.alert('[ERRO] O número deve ser entre 0 e 10.')
+            S.innerHTML = '<option>Digite um número acima entre 0 e 10</option>'
         }
     } else {
         window.alert('[ERRO] Por favor, é preciso que você insira um número.')
+        S.innerHTML = '<option>Digite um número acima</option>'
     }
 }
