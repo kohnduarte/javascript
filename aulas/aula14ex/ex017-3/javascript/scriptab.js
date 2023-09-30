c8f6215db7db8dca695e5447f43fb7883de9fa67
Erick Kohn <133143910+kohnduarte@users.noreply.github.com>
2023-09-30 13:02:28 -0300

    ex017 terminado pela terceira vez
    mais modificações foram feitas com o objetivo de praticar os diferentes tipos de estruturas de repetição. Nesse caso, a estrutura 'do/while'

---
+++ aulas/aula14ex/ex017-3/javascript/scriptab.js
@@ -0,0 +1,27 @@
+var B = window.document.querySelector('input[type="button"]')
+B.addEventListener('click', GerarTab)
+
+function GerarTab() {
+    var controle = window.document.getElementById('c1')
+    var S = window.document.getElementsByTagName('select')[0]
+    if (controle.value.length > 0) {
+        var n = Number.parseFloat(controle.value)
+        if (n >= -100 && n <= 100) {
+            var t = 1            
+            S.innerHTML = ''
+            // 'do/while' estrutura de repetição com teste lógico no final
+            do {
+                var opt = window.document.createElement('option')
+                opt.innerText = n + ' x ' + t + ' = ' + n*t
+                S.appendChild(opt)
+                t++
+            } while (t <= 10)
+        } else {
+            window.alert('[ERRO] Essa tabuada só funciona com números entre -100 e 100.')
+            S.innerHTML = '<option>Digite um número acima</option>'
+        }
+    } else {
+        window.alert('[ERRO] Faltam dados!')
+        S.innerHTML = '<option>Digite um número acima</option>'
+    }
+}
