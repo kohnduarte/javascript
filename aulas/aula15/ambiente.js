1ae98351096d86c80658c5e9576b09638f63db50
Erick Kohn <133143910+kohnduarte@users.noreply.github.com>
2023-10-03 13:32:04 -0300

    Create ambiente.js
    Vetores!

---
+++ aulas/aula15/ambiente.js
@@ -0,0 +1,12 @@
+let num = [5, 8, 2, 9, 3, 'Erick']
+num[1] = 7
+console.log(`Nosso vetor é o ${num}`)
+// o método .push() adiciona valores a um array, cria um elemento no final do array e coloca um valor dentro dele
+
+num.push(60)
+
+console.log(num[6])
+
+console.log(num.length)
+
+// o atributo '.length' informa o comprimento do array, a quantidade de valores que ele possui 
