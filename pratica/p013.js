9a1e0a2883c2c285d9c88e606f0757a1485bfffd
Erick Kohn <133143910+kohnduarte@users.noreply.github.com>
2023-10-03 21:15:45 -0300

    Create p013.js
    introdução a Array/Vetor

---
+++ pratica/p013.js
@@ -0,0 +1,40 @@
+// variável composta/vetor/array
+
+let n = [2, 6, 7, 9, 0, 45]
+console.log(n)
+console.log(`O meu vetor é ${n}`)
+n[6] = 22
+console.log(n)
+console.log(`O meu vetor é ${n}`)
+n.push(15) // push() - método para adicionar um elemento com um valor a um array
+console.log(n)
+console.log('O meu vetor é ' + n)
+n[0] = 1
+console.log(n)
+console.log(`Meu vetor: ${n}`)
+n[3] = 10
+console.log(`meu vetor: ${n}`)
+n.push('Erick')
+console.log(n)
+// o atributo '.length' indica o comprimento em elementos de um array, isto é, quanto elementos ele possui
+console.log(n.length)
+console.log(`O meu array possui ${n.length} elementos.`)
+n[20] = 44
+console.log(n)
+console.log(`meu array: ${n}`)
+n[6] = 13
+console.log(n)
+n.push('Kohn')
+console.log(n)
+console.log(n.length)
+n[10] = 26
+console.log(`meu vetor: ${n}`)
+console.log(n)
+n[12] = 34
+console.log(`meu vetor: ${n}`)
+n[16] = 50
+console.log(n)
+console.log('Meu vetor: ' + n)
+console.log(n.length)
+n.sort()
+console.log(n)
