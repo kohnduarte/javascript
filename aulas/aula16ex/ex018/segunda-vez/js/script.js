392341b401f6263450841ac48b3d6796821b181a
Erick Kohn <133143910+kohnduarte@users.noreply.github.com>
2023-10-20 11:30:09 -0300

    Update script.js

--- aulas/aula16ex/ex018/segunda-vez/js/script.js
+++ aulas/aula16ex/ex018/segunda-vez/js/script.js
@@ -1,20 +1,53 @@
 let controle = window.document.getElementById('c1')
 controle.focus() // uma alternativa para o atributo 'autofocus'
 let b1 = window.document.querySelector('input#botao1')
+let caixa = window.document.getElementsByTagName('select')[0]
 b1.addEventListener('click', Add)
 
-function Add() {
+valores = [] // um array é um conjunto ordenado de valores
+
+function vazio() {
     if (controle.value.length == 0) {
+        return false
+    } else {
+        return true
+    }
+}
+
+function ValidNum() {
+    if (parseInt(controle.value) < 1 || parseInt(controle.value) > 100) {
+        return false
+    } else {
+        return true
+    }
+}
+
+function naLista() {
+    if (valores.indexOf(parseInt(controle.value)) != -1) {
+        return false
+    } else {
+        return true
+    }
+}
+
+function Add() {
+    if (!vazio()) {
         window.alert('Por favor, digite um número.')
         controle.focus()
-    } else {
-        let number = parseInt(controle.value)
-        if (number < 1 || number > 100) {
+    } else if (!ValidNum()) {          
             window.alert('O número deve estar entre 1 e 100.')
             controle.value = ''
             controle.focus()
-        } else {
-            window.alert('ok')
-        }
-    }
+        } else if (!naLista()) {              
+            window.alert('Valor já encontrado na lista.')
+            controle.value = ''
+            controle.focus()
+            } else {
+                let number = parseInt(controle.value)
+                valores.push(number)
+                let item = window.document.createElement('option')
+                item.innerText = `Valor ${number} adicionado.`
+                caixa.appendChild(item)
+            }
 }
+    
