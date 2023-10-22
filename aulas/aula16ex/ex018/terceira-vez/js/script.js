8b1a8ae85aed1a6016b2d9651cd910b0d1d4a2b7
Erick Kohn <133143910+kohnduarte@users.noreply.github.com>
2023-10-22 11:13:38 -0300

    update

--- aulas/aula16ex/ex018/terceira-vez/js/script.js
+++ aulas/aula16ex/ex018/terceira-vez/js/script.js
@@ -1,8 +1,11 @@
 let controle = window.document.getElementById('c1')
 controle.focus()
+let seção = window.document.getElementsByClassName('secao')[0]
 let caixa = window.document.getElementsByName('selecao')[0]
 let bot1 = window.document.querySelector('input[value="Adicionar"]')
 let bot2 = window.document.getElementsByTagName('input')[2]
+let D = window.document.createElement('div')
+D.setAttribute('class', 'resultado')
 bot1.addEventListener('click', add)
 bot2.addEventListener('click', end)
 let vetor = []
@@ -69,7 +72,28 @@ function compMin(compvet, n) {
 
 function end() {
     if (compMin(vetor.length, 2)) {
-        window.alert('ok')
+        seção.appendChild(D)
+        D.innerHTML = `<p>Ao todo, temos ${vetor.length} números cadastrados.</p>`
+        /*
+        vetor.sort(function(a, b) {return b - a})
+        D.innerHTML += `<p>O maior valor informado foi ${vetor[0]}.</p>`
+        D.innerHTML += `<p>O menor valor informado foi ${vetor[vetor.length - 1]}.</p>`
+        */
+        let maior = 0
+        let menor = 99
+        let soma = 0
+        for (let i in vetor) {
+            if (maior < parseInt(vetor[i])) {
+                maior = vetor[i]
+            }
+            if (menor > parseInt(vetor[i])) {
+                menor = vetor[i]
+            }
+            soma = soma + parseInt(vetor[i])
+        }
+        D.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        D.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+        D.innerHTML += `<p>Somando todos os valores, temos ${soma}.`   
     } else {
         window.alert('É necessário haver pelo menos 2 números para prosseguir.')
         controle.focus()
