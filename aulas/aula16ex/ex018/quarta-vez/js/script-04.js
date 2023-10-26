ea7b522267e5ea4404e522dee2d2fad8c1350e98
Erick Kohn <133143910+kohnduarte@users.noreply.github.com>
2023-10-26 13:22:50 -0300

    Update script-04.js

--- aulas/aula16ex/ex018/quarta-vez/js/script-04.js
+++ aulas/aula16ex/ex018/quarta-vez/js/script-04.js
@@ -1,8 +1,12 @@
 let controle = window.document.getElementById('c1')
 controle.focus()
+let sect = window.document.getElementsByClassName('secao')[0]
 let S = window.document.getElementsByName('selecao')[0]
 let b1 = window.document.querySelector('input[value="Adicionar"]')
+let b2 = window.document.querySelector('input[value="Finalizar"]')
+let Dfinal = window.document.createElement('div')
 b1.addEventListener('click', Adic)
+b2.addEventListener('click', Final)
 let numbers = []
 
 function vazio(comp, zero) {
@@ -33,17 +37,43 @@ function Adic() {
     if (!vazio(controle.value.length, 0)) {
         if (numvalido(parseInt(controle.value), 1, 100)) {
             if (!presente(parseInt(controle.value), 0, numbers)) {
+                Dfinal.innerHTML = ''
                 numbers.push(parseInt(controle.value))
                 let item = window.document.createElement('option')
                 item.innerText = `Número ${controle.value} adicionado.`
                 S.appendChild(item)
+                controle.value = ''
+                controle.focus()
             } else {
                 window.alert('Este número já se encontra na lista. Por favor, escolha outro.')
+                controle.value = ''
+                controle.focus()
             }
         } else {
             window.alert('Apenas números entre 1 e 100 são permitidos.')
+            controle.value = ''
+            controle.focus()
         }
     } else {
         window.alert('Você esqueceu de digitar um número!')
+        controle.focus()
+    }
+}
+
+function completo(comp, min) {
+    if (comp >= min) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function Final() {
+    if (!completo(numbers.length, 2)) {
+        window.alert('É necessário haver pelo menos 2 números para avançar.')
+        controle.focus()
+    } else {
+        sect.appendChild(Dfinal)
+        Dfinal.innerHTML = `<p>Ao todo, temos ${numbers.length} números cadastrados.</p>`
     }
 }
