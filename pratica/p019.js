e998201cae573317c9e98fc551a07b1d8fa023e9
Erick Kohn <133143910+kohnduarte@users.noreply.github.com>
2023-10-25 21:20:08 -0300

    Update p019.js

--- pratica/p019.js
+++ pratica/p019.js
@@ -1,3 +1,4 @@
+/*
 let numbers = [65, 44, 12, 4]
 
 function myF(elmnt, index, arr) {
@@ -17,3 +18,32 @@ function fMulti(n, index, arr) {
 nums.forEach(fMulti)
 
 console.log(nums)
+*/
+
+let fruits = ['apple', 'orange', 'cherry']
+
+function myFunction(item, index) {
+    console.log(index + ': ' + item)
+}
+
+fruits.forEach(myFunction)
+
+let numbers = [65, 44, 12, 4]
+
+let soma = 0
+/*
+for (ind in numbers) {
+    soma += numbers[ind]
+}
+*/
+/*
+for (let ind = 0, c = numbers.length; ind < c; ind++) {
+    soma += numbers[ind]
+}
+*/
+
+numbers[5] = 70
+
+console.log(numbers)
+
+numbers.forEach(function(item) {console.log(item)})
