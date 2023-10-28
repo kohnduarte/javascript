4cfece723f57a1b0e2c3885cbb5583d25220779e
Erick Kohn <133143910+kohnduarte@users.noreply.github.com>
2023-10-28 13:27:34 -0300

    Update p021.js
    método sort () com função de comparação

--- pratica/p021.js
+++ pratica/p021.js
@@ -6,3 +6,17 @@ let fruits = ["Banana", "Orange", "Apple", "Mango"]
 console.log(fruits)
 let frutas = fruits.join(" and ")
 console.log(frutas)
+
+function funComp(c, d) {
+    var a = c.toLowerCase()
+    var b = d.toLowerCase()
+    if (a < b) {return -1}
+    if (a > b) {return 1}
+    return 0
+}
+
+let a = ['arroz', 'Batata', 'Ovos', 'feijão']
+a.sort()
+console.log(a)
+a.sort(funComp)
+console.log(a)
