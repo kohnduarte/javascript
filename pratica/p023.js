e62c6366c6118a28ba7df6e7bcdec43173dbd2f5
Erick Kohn <133143910+kohnduarte@users.noreply.github.com>
2023-11-01 14:00:54 -0300

    Update p023.js
    métodos slice( ) e splice( )

--- pratica/p023.js
+++ pratica/p023.js
@@ -1,3 +1,16 @@
+/*
 let a = [11, 3, 7, 4, 18, 9]
 a.sort((a, b) => b - a)
 console.log(a)
+*/
+
+let frutas = ['maçã', 'banana', 'uva', 'manga', 'mamão', 'laranja', 'tangerina', 'melão', 'abacate']
+console.log(frutas.slice(-4, -1)) // slice(start, end)
+
+let gosto = frutas.slice(-5, -2)
+
+console.log(gosto)
+
+let meses = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
+meses.splice(1)
+console.log(meses)
