6f88528de228e4c5a1c3f7b6b63686d4431ccddd
Erick Kohn <133143910+kohnduarte@users.noreply.github.com>
2023-11-04 13:12:39 -0300

    Update p027.js
    some( )

--- pratica/p027.js
+++ pratica/p027.js
@@ -3,3 +3,14 @@ var a = [24, 32, 44, 9, 16, 25, 0, 99, 35, 69, 4, 31, 40, 55, 78, 12, 15, 33, 71
 var nPositivos = function(el) {return el >= 0}
 
 console.log(a.every(nPositivos))
+
+let b = [1,2,3,4,5]
+let controle = 4
+
+let existe = b.some((el) => controle === el)
+
+if (existe == true) {
+    console.log('O número já existe')
+} else {
+    console.log('OK')
+}
