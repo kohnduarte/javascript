75dc86c55d1158109ae0a172d7331c820b136488
Erick Kohn <133143910+kohnduarte@users.noreply.github.com>
2023-11-03 21:57:46 -0300

    filter( )
    usando o método filter( ) para transformar arrays esparsos em densos e para remover elementos 'undefined' e 'null'

---
+++ pratica/p026.js
@@ -0,0 +1,19 @@
+let a = [11, 4, 7, 32, 69, 18, 45, 0, 21]
+delete a[0]
+delete a[5]
+delete a[7]
+//console.log(a)
+
+//a = a.filter(function(item) {return true})
+
+//console.log(a)
+
+a[0] = undefined 
+a[5] = null
+a[7] = undefined
+
+console.log(a)
+
+a = a.filter((x) => x != undefined && x != null)
+
+console.log(a)
