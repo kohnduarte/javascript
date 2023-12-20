7be8b71d162d2e050aee641489a0833648d46599
Erick Kohn <133143910+kohnduarte@users.noreply.github.com>
2023-12-20 13:50:04 -0300

    Update behavior.js

--- pratica/p038/script/behavior.js
+++ pratica/p038/script/behavior.js
@@ -7,5 +7,17 @@ function dieta() {
     var part = window.document.getElementsByName('participante')
     var cnum = window.document.getElementById('n')
 
+    if (radio[0].checked || radio[1].checked) {
+        if (part[0].checked || part[1].checked || part[2].checked || part[3].checked || part[4].checked) {
+            window.alert('ok')
+        } else {
+            window.alert('erro')
+        }
+    } else {
+        window.alert('Escolha se você quer engordar ou emagrecer os participantes.')
+    }
+
     
+
+
 }
