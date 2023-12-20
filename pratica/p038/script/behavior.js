9a49f71473cdd7b966fa94b23330bb18e28b8a65
Erick Kohn <133143910+kohnduarte@users.noreply.github.com>
2023-12-20 13:27:20 -0300

    update

--- pratica/p038/script/behavior.js
+++ pratica/p038/script/behavior.js
@@ -2,5 +2,7 @@ var botao = window.document.querySelector('input[type="button"]')
 botao.addEventListener('click', dieta)
 
 function dieta() {
-    
+    var peso = window.document.getElementsByClassName('weight')
+    var radio = window.document.getElementsByName('rad')
+    var part = window.document.getElementsByName('participante')
 }
